2a643df6895d7d329f3b6395684b289e00a2bf60
Justin Poliey <justin@getglue.com>
2012-10-20 05:51:17 -0400

    Inferring mRNA from Protein

---
+++ MRNA.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+from operator import mul
+
+
+def problem(dataset=None):
+    """
+    http://rosalind.info/problems/mrna/
+
+    Given: A protein string of length at most 50 aa.
+
+    Return: The total number of different RNA strings from which the protein
+    could have been translated.
+
+    """
+    if not dataset:
+        dataset = open("datasets/rosalind_mrna.txt").read()
+
+    amino_acids = defaultdict(list)
+    with open("codons.txt") as f:
+        for line in f:
+            if not line.strip():
+                continue
+            codon, aa = line.strip().split()
+            if aa == 'Stop':
+                aa = None
+            amino_acids[aa].append(codon)
+
+    pool = []
+    for aa in dataset:
+        pool.append(amino_acids[aa])
+    pool.append(amino_acids[None])
+    return reduce(mul, map(len, pool))
+
+
+if __name__ == '__main__':
+    print problem()
