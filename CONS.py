28527b64dade73fb36beb6d0e9bb1ef428ee086a
Justin Poliey <justin@getglue.com>
2012-10-20 02:42:04 -0400

    Consensus and Profile

---
+++ CONS.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from collections import Counter
+
+
+def problem(dataset=None):
+    """
+    http://rosalind.info/problems/cons/
+
+    Given: A collection of at most 10 DNA strings of equal length, at most 1
+    kbp.
+
+    Return: A consensus string and profile matrix for the collection. If
+    several possible consensus strings exist, then you may return any one of
+    them.
+
+    """
+    if not dataset:
+        dataset = open("datasets/rosalind_cons.txt").read()
+    dnas = [line.strip() for line in dataset.splitlines()]
+    counts = map(Counter, zip(*dnas))
+    consensus = [c.most_common(1)[0][0] for c in counts]
+    acids = ('A', 'C', 'G', 'T')
+    profile = dict((acid, [c.get(acid, 0) for c in counts]) for acid in acids)
+    return consensus, profile
+
+if __name__ == '__main__':
+    consensus, profile = problem()
+    print ''.join(consensus)
+    for acid in ('A', 'C', 'G', 'T'):
+        print "%s: %s" % (acid, ' '.join(map(str, profile[acid])))
