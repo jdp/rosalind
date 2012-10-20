a00a8afb75b84d60e4903f7f7316470fb56e4117
Justin Poliey <justin@getglue.com>
2012-10-20 01:32:25 -0400

    Reverse Complement

---
+++ reverse_complement.py
@@ -0,0 +1,22 @@
+from string import maketrans
+
+
+def problem(dataset=None):
+    """
+    In a DNA string, the complement of A is T, and the complement of C is G.
+
+    The reverse complement of a DNA string s is the string sc formed by
+    reversing the symbols of s, then taking the complement of each symbol
+    (e.g., the reverse complement of GTCA is TGAC).
+
+    Given: A DNA string s of length at most 1000 bp.
+
+    Return: The reverse complement of s.
+
+    """
+    if not dataset:
+        dataset = open("datasets/rosalind_revc.txt").read()
+    return dataset[::-1].translate(maketrans('ATCG', 'TAGC'))
+
+if __name__ == '__main__':
+    print problem('AAAACCCGGT')
