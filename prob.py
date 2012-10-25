8833136a8f831d787f2832702cb10100e0792355
Justin Poliey <justin@getglue.com>
2012-10-25 00:17:53 -0400

    clean up prob solution

--- prob.py
+++ prob.py
@@ -12,11 +12,20 @@ def problem(dataset=None):
     of three decimal places) that for the GC-content in A[i], two randomly
     chosen symbols will be the same.
 
+    >>> map(lambda x: round(x, 4), problem("0.23 0.31 0.75"))
+    [0.3229, 0.2861, 0.3125]
+
     """
     if not dataset:
         dataset = open("datasets/rosalind_prob.txt").read()
+
+    # Parse dataset
     A = map(float, dataset.split())
+
+    # Return probabilities
     return [((x ** 2) + ((1 - x) ** 2)) / 2 for x in A]
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
     print ' '.join(map(str, problem()))
