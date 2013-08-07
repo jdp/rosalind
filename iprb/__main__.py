0adf918a4438b81ca89042f1b753d88e8f14ba41
Justin Poliey <justin@getglue.com>
2013-08-07 00:27:16 -0400

    better solution for iprb

--- iprb/__main__.py
+++ iprb/__main__.py
@@ -11,25 +11,28 @@ dominant phenotype). Assume that any two organisms can mate.
 0.78333
 """
 
-import os
-from itertools import chain, combinations, product
-
-
-def mate(a, b):
-    return map(''.join, product(a, b))
-
-
 def problem(k, m, n):
-    pool = ["AA"] * k + ["Aa"] * m + ["aa"] * n
-    possible = list(chain(*[mate(a, b) for a, b in combinations(pool, 2)]))
-    dominants = [p for p in possible if "A" in p]
-    return float(len(dominants)) / len(possible)
+    k, m, n = map(float, (k, m, n))
+    p = [
+        k * (k - 1),  # AA, AA pairs
+        k * m,  # AA, Aa pairs
+        k * n,  # AA, aa pairs
+        m * k,  # Aa, AA pairs
+        m * (m - 1) * 0.75, # Aa, Aa pairs
+        m * n * 0.5, # Aa, aa pairs
+        n * k,  # aa, AA pairs
+        n * m * 0.5,  # aa, Aa pairs
+        0,  # aa, aa pairs
+    ]
+    t = k + m + n
+    return sum(p) / t / (t - 1)
 
 
 if __name__ == '__main__':
     import doctest
+    import os
     doctest.testmod()
 
     dataset = open(os.path.dirname(__file__) + "/rosalind_iprb.txt").read()
-    k, m, n = map(int, dataset.strip().split())
+    k, m, n = map(float, dataset.strip().split())
     print problem(k, m, n)
